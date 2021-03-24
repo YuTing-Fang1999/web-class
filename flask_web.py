c1d009a96088a3dcd85a5252c2000cc3488d6040
YuTing-Fang1999 <4107056005@smail.nchu.edu.tw>
2021-03-24 17:59:56 +0800

    upload

---
+++ flask_web.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('login.html')
+
+@app.route('/get_login_info')
+def get_login_info():
+   data = request.args
+   return data
+
+if __name__ == '__main__':
+   app.debug = True
+   app.run()
